4238c0f550de58478ae834e746a27f0b802842e9
stult_i <extinbase@gmail.com>
2019-07-10 15:43:16 +0900

    Controller CORS change

--- src/main/java/com/stulti/go4thetop/HomeController.java
+++ src/main/java/com/stulti/go4thetop/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
+@CrossOrigin(origins = {"http://localhost:3000", "https://www.go4thetop.net"}, maxAge = 3600)
 @Controller
 public class HomeController {
 
